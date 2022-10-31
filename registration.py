26f59ffba228bf413d6129752e23012dee8e5cd4
Inkogni7o <ezykov25@gmail.com>
2022-10-31 18:00:07 +0300

    configure register file

--- registration.py
+++ registration.py
@@ -1,3 +1,6 @@
+# -*- coding: UTF-8 -*-
+import sqlite3
+
 from PyQt5.QtWidgets import QWidget
 from forms.reg import Ui_Form
 
@@ -6,3 +9,12 @@ class RegWindow(QWidget, Ui_Form):
     def __init__(self):
         super(RegWindow, self).__init__()
         self.setupUi(self)
+        self.lets_begin_btn.clicked.connect(self.new_user)
+
+    def new_user(self):
+        with sqlite3.connect('db/users_db.db') as con:
+            cur = con.cursor()
+            cur.execute("""INSERT INTO users(login,password,name,second_name) VALUES(?,?,?,?)""",
+                        (self.login_input.text(), self.password.text(), self.name_input.text(),
+                         self.name2_input.text()))
+            con.commit()
