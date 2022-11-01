1b1f0d5ba34048ded8fbc42c2129bc2166851f94
Inkogni7o <ezykov25@gmail.com>
2022-11-01 20:41:19 +0300

    reworked choose day system

--- new_group.py
+++ new_group.py
@@ -12,6 +12,28 @@ class NewGroupWindow(QWidget, Ui_Form):
         self.buttonBox.rejected.connect(lambda: self.close())
         self.buttonBox.accepted.connect(self.new_group)
 
+        lst = [self.monday, self.tuesday, self.wendsday, self.thursday, self.friday, self.saturday, self.sunday]
+        for word in lst:
+            word.clicked.connect(self.open_time)
+
+        self.main_dict = {
+            'Понедельник': (self.time_edit_1, self.end_edit_1),
+            'Вторник': (self.time_edit_2, self.end_edit_2),
+            'Среда': (self.time_edit_3, self.end_edit_3),
+            'Четверг': (self.time_edit_4, self.end_edit_4),
+            'Пятница': (self.time_edit_5, self.end_edit_5),
+            'Суббота': (self.time_edit_6, self.end_edit_6),
+            'Воскресенье': (self.time_edit_7, self.end_edit_7)
+        }
+
+    def open_time(self):
+        if self.main_dict[self.sender().text()][0].isEnabled() == 0:
+            self.main_dict[self.sender().text()][0].setEnabled(True)
+            self.main_dict[self.sender().text()][1].setEnabled(True)
+        else:
+            self.main_dict[self.sender().text()][0].setEnabled(False)
+            self.main_dict[self.sender().text()][1].setEnabled(False)
+
     def new_group(self):
         pass
 
