e7e9ec8b91c26c1b03c47c4897b9a4e249cfa6ff
Inkogni7o <ezykov25@gmail.com>
2022-11-02 18:07:07 +0300

    add config group system

---
+++ edit_group.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+import sqlite3
+
+from PyQt5.QtWidgets import QMainWindow
+from forms.one_groupUI import Ui_MainWindow
+
+
+class EditGroupWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, id_group: int):
+        super(EditGroupWindow, self).__init__()
+        self.setupUi(self)
+        with sqlite3.connect('db/main_db.db') as con:
+            cur = con.cursor()
+            self.result = cur.execute(""" """)
--- today_activity.py
+++ today_activity.py
@@ -5,11 +5,12 @@ import sys
 
 from PyQt5.QtCore import QDate
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem, QHeaderView
 from forms.today_formUI import Ui_MainWindow
 from new_group import NewGroupWindow
 from consts import DECODE_DAYS
+from edit_group import EditGroupWindow
 
 
 class TodayWindow(QMainWindow, Ui_MainWindow):
@@ -26,17 +27,28 @@ class TodayWindow(QMainWindow, Ui_MainWindow):
         with sqlite3.connect('db/main_db.db') as con:
             day_of_the_week = DECODE_DAYS[self.calendar.selectedDate().dayOfWeek()]
             cur = con.cursor()
-            result = cur.execute("""SELECT title, days_of_the_week, starts, ends FROM groups
-             WHERE days_of_the_week LIKE ?""",
-                                 ('%' + day_of_the_week + '%',)).fetchall()
-            for group in result:
-                self.today_groups.setRowCount(len(result))
-                self.today_groups.setItem(result.index(group), 0, QTableWidgetItem(group[0]))
-                time = QTableWidgetItem(group[2].split()[group[1].split().index(day_of_the_week)] + ' - '
-                                        + group[3].split()[group[1].split().index(day_of_the_week)])
-                self.today_groups.setItem(result.index(group), 1, time)
-                self.today_groups.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-                self.today_groups.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+            self.result = cur.execute("""SELECT title, days_of_the_week, starts, ends, id FROM groups
+             WHERE days_of_the_week LIKE ?""", ('%' + day_of_the_week + '%',)).fetchall()
+        for group in self.result:
+            self.today_groups.setRowCount(len(self.result))
+            self.today_groups.setItem(self.result.index(group), 0, QTableWidgetItem(group[0]))
+            time = QTableWidgetItem(group[2].split()[group[1].split().index(day_of_the_week)] + ' - '
+                                    + group[3].split()[group[1].split().index(day_of_the_week)])
+            self.today_groups.setItem(self.result.index(group), 1, time)
+            self.today_groups.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+            self.today_groups.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+            self.today_groups.scrollToItem(time)
+        self.today_groups.viewport().installEventFilter(self)
+
+    def eventFilter(self, source, event):
+        if (event.type() == QtCore.QEvent.MouseButtonDblClick and
+                event.buttons() == QtCore.Qt.LeftButton and
+                source is self.today_groups.viewport()):
+            item = self.today_groups.itemAt(event.pos())
+            self.edit_group = EditGroupWindow(self.result[-1])
+            self.close()
+            self.edit_group.show()
+        return super(TodayWindow, self).eventFilter(source, event)
 
     def add_new_group(self):
         wndw = NewGroupWindow(self.login)
